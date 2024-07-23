77e159a2f68c9612b36818adb2cb153c71577c34
kimjuo <kimjuo1097@gmail.com>
2024-07-23 20:29:58 +0900

    test add

--- lib/screen/admin/adminScreen/adminCheckScreen.dart
+++ lib/screen/admin/adminScreen/adminCheckScreen.dart
@@ -14,7 +14,7 @@ class _AdminCheckLoadingScreenState extends State<AdminCheckLoadingScreen> {
     return const Center(
       child: Column(
         children: [
-
+          
         ],
       ),
     );
